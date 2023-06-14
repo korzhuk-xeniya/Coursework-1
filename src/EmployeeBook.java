b762c79b13d26638a3741fd0e07b621b81f50dbb
Ксения <korzhuk.xeniya@yandex.ru>
2023-06-14 21:36:53 +0300

    Очень сложно 4a+4b done

---
+++ src/EmployeeBook.java
@@ -0,0 +1,29 @@
+public class EmployeeBook {
+    private final Employee[] employees;
+    private int size;
+
+    public EmployeeBook() {
+        this.employees = new Employee[10];
+    }
+
+    public void addEmployee(FIO fio, int department, int salary, int id) {
+        if (size >= employees.length) {
+            System.out.println("Нельзя добавить сотрудника, в книге закончилось место");
+        }
+        for (int lineOfBook = 0; lineOfBook < size; lineOfBook++) {
+            if (employees[lineOfBook] == null) {
+                Employee newEmployee = new Employee(fio, department, salary);
+                employees[size++] = newEmployee;
+            }
+        }
+    }
+    public void deleteEmployee (FIO fio) {
+        for (int lineOfBook = 0; lineOfBook < size; lineOfBook++) {
+            if (employees[lineOfBook].getFio().equals(fio)) {
+                System.out.println(employees[lineOfBook].getFio() + " удален ");
+                employees[lineOfBook] = null;
+                size--;
+            }
+        }
+    }
+}
