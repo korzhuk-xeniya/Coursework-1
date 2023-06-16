6126d2801466255569f8c1a3ea63d350643ab377
Ксения <korzhuk.xeniya@yandex.ru>
2023-06-16 23:37:51 +0300

    Повышенная сложность 2-b done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static int countSumPayForMonth(Employee[] employees) {
@@ -10,7 +11,7 @@ public class Main {
         return sum;
     }
 
-    public static String findMinSalary(Employee[] employees) {
+    public static FIO findEmployeeWithMinSalary(Employee[] employees) {
         int minSalary = employees[0].getSalary();
         FIO fio = employees[0].getFio();
         for (int cell = 0; cell < employees.length; cell++) {
@@ -19,10 +20,22 @@ public class Main {
                 fio = employees[cell].getFio();
             }
         }
-        return "Сотрудник с минимальной зарплатой - " + minSalary + ": " + fio;
+        return fio;
     }
 
-    public static String findMaxSalary(Employee[] employees) {
+    public static int findMinSalary(Employee[] employees) {
+        int minSalary = employees[0].getSalary();
+        FIO fio = employees[0].getFio();
+        for (int cell = 0; cell < employees.length; cell++) {
+            if (employees[cell].getSalary() < minSalary) {
+                minSalary = employees[cell].getSalary();
+                fio = employees[cell].getFio();
+            }
+        }
+        return minSalary;
+    }
+
+    public static FIO findEmployeeWithMaxSalary(Employee[] employees) {
         int maxSalary = employees[0].getSalary();
         FIO fio = employees[0].getFio();
         for (int cell = 0; cell < employees.length; cell++) {
@@ -31,7 +44,19 @@ public class Main {
                 fio = employees[cell].getFio();
             }
         }
-        return "Сотрудник с максимальной зарплатой - " + maxSalary + ": " + fio;
+        return fio;
+    }
+
+    public static int findMaxSalary(Employee[] employees) {
+        int maxSalary = employees[0].getSalary();
+        FIO fio = employees[0].getFio();
+        for (int cell = 0; cell < employees.length; cell++) {
+            if (employees[cell].getSalary() > maxSalary) {
+                maxSalary = employees[cell].getSalary();
+                fio = employees[cell].getFio();
+            }
+        }
+        return maxSalary;
     }
 
     public static float countAverageValueOfSalaries(Employee[] employees) {
@@ -39,12 +64,46 @@ public class Main {
         return averageSalary;
     }
 
-    public static String writeFullNames (Employee[]employees) {
-        FIO []fio = new FIO[employees.length];
+    public static String writeFullNames(Employee[] employees) {
+        FIO[] fio = new FIO[employees.length];
         for (int cell = 0; cell < employees.length; cell++) {
             fio[cell] = employees[cell].getFio();
         }
         return Arrays.toString(fio);
+    }
+
+    public static void increaseSalary(Employee[] employees, int percent) {
+        for (int cell = 0; cell < employees.length; cell++) {
+            employees[cell].setSalary(employees[cell].getSalary() + employees[cell].getSalary() * percent / 100);
+        }
+    }
+
+    public static FIO findEmployeeWithMinSalaryInDepartment(int department, Employee[] employees) {
+        int minSalaryInDepartment = findMaxSalary(employees);
+        FIO fio = findEmployeeWithMaxSalary(employees);
+        for (int cell = 0; cell < employees.length; cell++) {
+            if (employees[cell].getDepartment() == department) {
+                if (employees[cell].getSalary() < minSalaryInDepartment) {
+                    minSalaryInDepartment = employees[cell].getSalary();
+                    fio = employees[cell].getFio();
+                }
+            }
+        }
+        return fio;
+    }
+
+    public static FIO findEmployeeWithMaxSalaryInDepartment(int department, Employee[] employees) {
+        int maxSalaryInDepartment = findMinSalary(employees);
+        FIO fio = findEmployeeWithMinSalary(employees);
+        for (int cell = 0; cell < employees.length; cell++) {
+            if (employees[cell].getDepartment() == department) {
+                if (employees[cell].getSalary() > maxSalaryInDepartment) {
+                    maxSalaryInDepartment = employees[cell].getSalary();
+                    fio = employees[cell].getFio();
+                }
+            }
+        }
+        return fio;
     }
 
     public static void main(String[] args) {
@@ -86,10 +145,24 @@ public class Main {
         }
 
         System.out.println("Сумма затрат на зарплаты в месяц: " + countSumPayForMonth(employees));
-        System.out.println(findMinSalary(employees));
-        System.out.println(findMaxSalary(employees));
+        System.out.println("Сотрудник с минимальной зарплатой - " + findEmployeeWithMinSalary(employees));
+        System.out.println("Сотрудник с максимальной зарплатой - " + findEmployeeWithMaxSalary(employees));
         System.out.println("Среднее значение зарплат: " + countAverageValueOfSalaries(employees));
         System.out.println(writeFullNames(employees));
+        increaseSalary(employees, 10);
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println(employees[i]);
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите номер отдела (от 1 до 5)");
+        int department = scanner.nextInt();
+        if (department > 0 && department <= 5) {
+            System.out.println("Сотрудник с минимальной зарплатой в " + department + " отделе: " + findEmployeeWithMinSalaryInDepartment(department, employees));
+            System.out.println("Сотрудник с максимальной зарплатой в " + department + " отделе: " + findEmployeeWithMaxSalaryInDepartment(department, employees));
+        } else {
+            System.out.println("Такого отдела не существует");
+        }
+
     }
 }
 
